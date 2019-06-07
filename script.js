5df424a50256a96fccfa14b1e90153fa5568f552
Maria <44950211+MariaModan@users.noreply.github.com>
2019-06-07 22:31:08 +0100

    added css and fixed the countdown

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-const startTime = (new Date()).getTime();
-const min = 0;
-const sec = 5;
-const endTime = startTime+ min*60000 +sec*1000;
 
-var dif = endTime - startTime;
+const button = document.getElementById("btn");
 
-function msToTime(ms){
-  var min = Math.floor(ms / 60000);
-  var sec = Math.floor((ms-min*60000)/ 1000);
-  return min + ":" + sec;
-}
+button.addEventListener("click", function(){
+  const min = document.getElementById("min").value;
+  const sec = document.getElementById("sec").value;
 
-function countDown(num){
-  let currentTime = (new Date()).getTime();
-  let clock = endTime - currentTime;
-  if (clock == 0){
-    clearInterval(timer);
+  const startTime = (new Date()).getTime();
+  const endTime = startTime+ min*60000 +sec*1000;
+  var dif = endTime - startTime;
+
+  function msToTime(ms){
+    var min = Math.floor(ms / 60000);
+    var sec = Math.floor((ms-min*60000)/ 1000);
+    return min + ":" + sec;
   }
-  console.log(msToTime(clock));
-  return currentTime;
-}
 
-let timer = setInterval(countDown, 1000);
+  function countDown(num){
+    let currentTime = (new Date()).getTime();
+    let clock = endTime - currentTime;
+    console.log(clock);
+    if (clock < 1000){
+      clearInterval(timer);
+    }
+    console.log(msToTime(clock));
+
+    let newSec = msToTime(clock).split(":")[1];
+    if (newSec.length ===1 ){
+      newSec = '0' + newSec;
+    }
+    let newMin = msToTime(clock).split(":")[0];
+    if (newMin.length ===1 ){
+      newMin = '0' + newMin;
+    }
+
+
+    document.getElementById("sec").value = newSec;
+    document.getElementById("min").value = newMin;
+
+    return msToTime(clock);
+  }
+
+
+  let timer = setInterval(countDown, 1000);
+});
